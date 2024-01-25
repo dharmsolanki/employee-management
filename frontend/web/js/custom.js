aa6a3e22231157c624f44ecbb328f799f4452765
dharmsolanki <dharmsolanki1010@gmail.com>
2024-01-25 17:18:52 +0530

    fix console error for show password checkbox

--- frontend/web/js/custom.js
+++ frontend/web/js/custom.js
@@ -24,18 +24,29 @@ document.addEventListener("DOMContentLoaded", function () {
   let showPasswordCheckbox = document.getElementById("showPasswordCheckboxId");
 
   function togglePasswordFields() {
+    if (!showPasswordCheckbox) {
+      console.error("Show Password checkbox not found.");
+      return;
+    }
+
     let passwordFieldIds = ["signupform-password", "loginform-password"];
 
     passwordFieldIds.forEach(function (fieldId) {
       let passwordField = document.getElementById(fieldId);
       if (passwordField) {
         passwordField.type = showPasswordCheckbox.checked ? "text" : "password";
+      } else {
+        // console.error("Password field with ID '" + fieldId + "' not found.");
       }
     });
   }
 
-  showPasswordCheckbox.addEventListener("change", togglePasswordFields);
+  if (showPasswordCheckbox) {
+    showPasswordCheckbox.addEventListener("change", togglePasswordFields);
 
-  // Trigger the initial state
-  togglePasswordFields();
+    // Trigger the initial state
+    togglePasswordFields();
+  } else {
+    // console.error("Show Password checkbox not found.");
+  }
 });
